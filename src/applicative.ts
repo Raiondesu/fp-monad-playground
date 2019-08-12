942c39774c8b342b3a3c7743159322a57eb28498
Raiondesu <a.raiondesu@gmail.com>
2019-08-12 20:05:45 +0300

    Fix [isApplicable] applicative

--- src/applicative.ts
+++ src/applicative.ts
@@ -2,7 +2,7 @@ type Apply<T, This> = T extends (x: infer FT) => infer N
   ? <F extends Functor<FT>>(f: F) => Functor<N>
   : (f: any) => This;
 
-const isApplicable = <T = any, R = any>(v: any): v is (x: T) => R => typeof v === 'function';
+const isApplicable = <T = any, R = any>(v: any): v is ((x: T) => R) => typeof v === 'function';
 
 interface Applicative<T> extends Functor<T> {
   apply: Apply<T, this>;
