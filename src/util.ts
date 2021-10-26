87eabe923618408df570a21d10c7904de1131532
Alexey Iskhakov <a.raiondesu@gmail.com>
2021-10-26 22:30:15 +0300

    Add `includes` implementation

--- src/util.ts
+++ src/util.ts
@@ -7,6 +7,14 @@ interface Concat {
 const concat = <A extends Concat>(a: A) => <B extends A>(b: B): B => a.concat(b);
 const append = <A extends Concat>(a: A) => <B extends A>(b: B): B => b.concat(a);
 
-interface Includes {
-  includes(value: any): boolean;
+interface Includes<V> {
+  includes(value: V): boolean;
 }
+
+const includes = <P>(part: P) => (whole: Includes<P>) => whole.includes(part);
+
+const partOf = <I extends Includes<any>>(
+  a: I
+) => <B extends I extends Includes<infer U> ? U : any>(
+  b: B
+) => a.includes(b);
